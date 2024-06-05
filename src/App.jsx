6f7a2712eb83d6172c09adad9300c7e8e01663de
danielor92 <danielospinar@gmail.com>
2024-06-05 00:21:30 -0500

    Theme Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+import { Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar  from "./scenes/global/Topbar";
@@ -22,6 +24,18 @@ function App() {
 				<div className="app">
 					<main className="content">
 						<Topbar />
+						<Routes>
+							<Route path="/dashboard" element={<Dashboard />}/>
+							<Route path="/team" element={<Team />}/>
+							<Route path="/invoices" element={<Invoices />}/>
+							<Route path="/contacts" element={<Contacts />}/>
+							<Route path="/bar" element={<Bar />}/>
+							<Route path="/form" element={<Form />}/>
+							<Route path="/line" element={<Line />}/>
+							<Route path="/pie" element={<Pie />}/>
+							<Route path="/FAQ" element={<FAQ />}/>
+							<Route path="/geography" element={<Geography />}/>
+						</Routes>
 					</main>
 				</div>
 			</ThemeProvider>
